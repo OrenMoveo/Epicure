01586086b206d59baa325614bc42637d212e9cf4
Oren Partovi <orenpartovi@Oren-Partovi.local>
2024-01-22 13:40:15 +0200

    navbar flex layout fixed, app parent in main.jsx fixed

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.scss";
+import Home from "./pages/Home/Home.tsx";
+function App() {
+  return <Home />;
+}
+
+export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,30 +2,32 @@ import React from "react";
 import "./Navbar.scss";
 function Navbar() {
   return (
-    <div className="header-container">
-      <div className="menu-container">
-        <img src="src/assets/images/HamburgIcon.svg" alt="hambur" />
+    <section className="header-container">
+      <div className="navbar-wrapper">
+        <div className="navbar-container">
+          <div className="menu-container">
+            <img src="src/assets/images/HamburgIcon.svg" alt="hambur" />
+          </div>
+          <div className="logo-container">
+            <img src="src/assets/images/logo.svg" alt="logo" />
+          </div>
+          <div className="epicure-text-title">EPICURE</div>
+          <div className="restaurants-navbar-button">Restaurants</div>
+          <div className="chefs-navbar-button">Chefs</div>
+        </div>
+        <div className="icons-container">
+          <div className="search-button">
+            <img src="src/assets/images/SearchIcon.svg" alt="search-icon" />
+          </div>
+          <div className="signIn-button">
+            <img src="src/assets/images/SignInIcon.svg" alt="signIn-icon" />
+          </div>
+          <div className="bag-button">
+            <img src="src/assets/images/BagIcon.svg" alt="bag-icon" />
+          </div>
+        </div>
       </div>
-      <div className="navbar-container">
-        <div className="logo-container">
-          <img src="src/assets/images/logo.svg" alt="logo" />
-        </div>
-        <div className="epicure-text-title">EPICURE</div>
-        <div className="restaurants-navbar-button">Restaurants</div>
-        <div className="chefs-navbar-button">Chefs</div>
-      </div>
-      <div className="search-signin-bag-container">
-        <div className="search-button">
-          <img src="src/assets/images/SearchIcon.svg" alt="search-icon" />
-        </div>
-        <div className="signin-button">
-          <img src="src/assets/images/SignInIcon.svg" alt="signin-icon" />
-        </div>
-        <div className="bag-button">
-          <img src="src/assets/images/BagIcon.svg" alt="bag-icon" />
-        </div>
-      </div>
-    </div>
+    </section>
   );
 }
 
