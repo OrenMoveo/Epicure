551109bb354dfb3947b7e1a6aca1c4c6437484bf
Oren Partovi <orenpartovi@Oren-Partovi.local>
2024-01-23 14:05:13 +0200

    restaurant card style is done

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Dish, Restaurant } from "../../types/types";
+import "./Card.scss";
 interface CardProps {
   cardType: string;
   pictureUrl: string;
@@ -9,15 +10,17 @@ interface CardProps {
 }
 function Card({
   content,
-  pictureUrl: pictureUrl,
+  pictureUrl,
   title,
   cardType,
   isHorizontal,
 }: CardProps) {
+  console.log(pictureUrl);
+
   return (
     <div className="card">
       <div className="card-img">
-        <img src={pictureUrl} alt={cardType} />
+        <img src={pictureUrl} alt={title + " " + cardType} />
       </div>
       <div
         className={
@@ -26,12 +29,14 @@ function Card({
             : "dish-card-container"
         }
       >
+        {/* <div className="card-content-wrapper"> */}
         <h4 className="card-title">{title}</h4>
         {cardType === "restaurant" ? (
           <p className="restaurant-card-content">{content}</p>
         ) : (
           <div className="dish-card-content">{content}</div>
         )}
+        {/* </div> */}
       </div>
     </div>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
+import RestaurantCarousell from "../../components/RestaurantCarousell/RestaurantCarousell";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <RestaurantCarousell />
     </>
   );
 }
