6ffdc7e5e6c9f276cc22e0ad37238f33b061a23c
Oren Partovi <orenpartovi@Oren-Partovi.local>
2024-01-24 10:30:01 +0200

    footer mobile is done but to small fix in flex gap in buttons-container

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.scss";
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="buttons-container">
+        <button className="contact-us-button" name="contact-us-button">
+          Contact Us
+        </button>
+        <button className="term-of-use-button" name="term-of-use-button">
+          Term Of Use
+        </button>
+        <button className="privacy-policy-button" name="privacy-policy-button">
+          Privacy Policy
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
