919f685a0d125d08b7d6d55cd053958322586a55
Nitzan Papini <nitzanp@moveo.co.il>
2024-01-25 17:29:03 +0200

    Revert "Feature/about section"

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
-import AboutSection from "../../components/AboutSection/AboutSection";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <AboutSection />
       <Footer />
     </>
   );
