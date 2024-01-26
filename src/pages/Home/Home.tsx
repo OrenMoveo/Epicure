538242b5ff332f6040488ae106f8a14a6cd1200c
Oren Partovi <orenp@moveo.co.il>
2024-01-26 11:48:14 +0200

    chef of the week html section strucutre build

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
 import AboutSection from "../../components/AboutSection/AboutSection";
+import { ChefOfTheWeekSection } from "../../components/ChefOfTheWeekSection/ChefOfTheWeekSection";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <ChefOfTheWeekSection />
       <AboutSection />
       <Footer />
     </>
