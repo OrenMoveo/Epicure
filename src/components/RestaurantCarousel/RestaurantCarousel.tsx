8a23a68995b7188d2ba6a59a5cab875c6e87722f
Oren Partovi <orenp@moveo.co.il>
2024-01-28 17:40:43 +0200

    Restuarant carousel structure done

--- src/components/RestaurantCarousel/RestaurantCarousel.tsx
+++ src/components/RestaurantCarousel/RestaurantCarousel.tsx
@@ -4,7 +4,13 @@ import data from "../../data/data.json";
 import { Restaurant } from "../../types/types";
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import { SectionTitle } from "../SectionTitle/SectionTitle";
+import { useMediaQuery } from "react-responsive";
+
 const RestaurantCarousel = () => {
+  const isMobile: boolean = useMediaQuery({
+    query: "(max-width: 450px)",
+  });
+
   const restaurants: Restaurant[] = data.data.restaurants;
   return (
     <div className="restaurant-carousel-container">
@@ -14,8 +20,15 @@ const RestaurantCarousel = () => {
       <div className="restaurant-carousel-content">
         <RestaurantCard restaurant={restaurants[0]} />
       </div>
+      {isMobile && (
+        <div className="all-restaurants-goto-container">
+          <button className="all-restaurants-button">
+            All Restaurants
+            <img src="/src/assets/images/goToIcon.svg" alt="goToIcon" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
-
 export default RestaurantCarousel;
