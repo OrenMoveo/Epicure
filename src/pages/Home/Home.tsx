bab2cb9c75cb1200204b821bc4f1af59b5d82ed5
Oren Partovi <orenp@moveo.co.il>
2024-01-28 13:54:13 +0200

    Legend done for mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,12 +4,14 @@ import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
 import AboutSection from "../../components/AboutSection/AboutSection";
 import { ChefOfTheWeekSection } from "../../components/ChefOfTheWeekSection/ChefOfTheWeekSection";
+import Legend from "../../components/Legend/Legend";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Legend />
       <ChefOfTheWeekSection />
       <AboutSection />
       <Footer />
