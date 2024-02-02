a6158b6eec9e63661b8e604ea5dd5039e8a5b3ae
Oren Partovi <orenp@moveo.co.il>
2024-02-02 16:16:12 +0200

    Restaurants Page for desktop is set, TODO filters

--- src/pages/RestaurantsPage/RestaurantsPage.tsx
+++ src/pages/RestaurantsPage/RestaurantsPage.tsx
@@ -1,21 +1,36 @@
 import RestaurantCard from "../../components/RestaurantCard/RestaurantCard";
 import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
 import { useFetch } from "../../hooks/useFetch";
+import useIsMobile from "../../hooks/useIsMobile";
+import useIsTablet from "../../hooks/useIsTablet";
 import { Restaurant } from "../../types/types";
 import styles from "./RestaurantsPage.module.scss";
 
 const RestaurantsPage = () => {
   const data = useFetch();
   const restaurants: Restaurant[] = data.restaurants;
+  const isTablet = useIsTablet();
+  const isMobile = useIsMobile();
+  const isMobileOrTable = isMobile || isTablet;
 
   return (
     <section className={styles.RestaurantsPageSection}>
       <div className={styles.restaurantsPageLayout}>
         <div className={styles.restaurantsPageContentContainer}>
-          <div className={styles.restaurantPageTitleContainer}>
-            <SectionTitle title={"RESTAURANTS"} />
-          </div>
-          <div className={styles.filtersContainer}>Filters</div>
+          {isMobileOrTable && (
+            <div className={styles.restaurantPageTitleContainer}>
+              <SectionTitle title={"RESTAURANTS"} />
+            </div>
+          )}
+          {isMobileOrTable ? (
+            <div className={styles.dropDownsFiltersContainer}>Filters</div>
+          ) : (
+            <div className={styles.dropDownsFiltersContainer}>
+              <div>Price Range</div>
+              <div>Distance</div>
+              <div>Rating</div>
+            </div>
+          )}
           <div className={styles.restaurantsCardsContainer}>
             {restaurants.map((restaurant) => (
               <RestaurantCard restaurant={restaurant} key={restaurant.keyId} />
