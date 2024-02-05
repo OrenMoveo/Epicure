0402307c6d321a512142664df99ceb15f456e4ee
Oren Partovi <orenp@moveo.co.il>
2024-02-05 12:00:10 +0200

    homepage and restaurants page navigation fixed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,21 +4,41 @@ import logoIcon from "../../assets/images/logo.svg";
 import searchIcon from "../../assets/images/SearchIcon.svg";
 import signInIcon from "../../assets/images/SignInIcon.svg";
 import bagIcon from "../../assets/images/BagIcon.svg";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const handleHomePageNavigation = () => {
+    navigate("/");
+  };
+
+  const handleRestaurantsPageNavigation = () => {
+    navigate("/restaurants");
+  };
+
   return (
     <section className="header-container">
       <div className="header-wrapper">
         <div className="navbar-container">
           <div className="menu-container">
-            <img src={hamburgerIcon} alt="hambur" />
+            <img src={hamburgerIcon} alt="hamburger-icon" />
           </div>
-          <div className="logo-container">
+          <button className="logo-container" onClick={handleHomePageNavigation}>
             <img src={logoIcon} alt="logo" />
-          </div>
+          </button>
           <div className="navbar-buttons-container">
-            <div className="epicure-text-title">EPICURE</div>
-            <div className="restaurants-navbar-button">Restaurants</div>
+            <button
+              className="epicure-text-title"
+              onClick={handleHomePageNavigation}
+            >
+              EPICURE
+            </button>
+            <button
+              className="restaurants-navbar-button"
+              onClick={handleRestaurantsPageNavigation}
+            >
+              Restaurants
+            </button>
             <div className="chefs-navbar-button">Chefs</div>
           </div>
         </div>
