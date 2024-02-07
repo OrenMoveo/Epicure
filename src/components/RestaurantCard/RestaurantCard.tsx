b6588707d1754abb67594257969f13bd7b068d95
Oren Partovi <orenp@moveo.co.il>
2024-02-07 13:11:15 +0200

    absolute path fix

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -2,6 +2,12 @@ import styles from "./RestaurantCard.module.scss";
 import { Restaurant } from "../../types/types";
 import useGetScreenWidth from "../../hooks/useGetWidthScreen";
 import { UIConstants } from "../../shared/constants";
+import oneStarRating from "../../assets/images/ratings/rating1.svg";
+import twoStarRating from "../../assets/images/ratings/rating2.svg";
+import threeStarRating from "../../assets/images/ratings/rating3.svg";
+import fourStarRating from "../../assets/images/ratings/rating4.svg";
+import fiveStarRating from "../../assets/images/ratings/rating5.svg";
+
 interface RestaurantCardProps {
   restaurant: Restaurant;
   className?: string;
@@ -10,14 +16,15 @@ interface RestaurantCardProps {
 const RestaurantCard = ({ restaurant, className }: RestaurantCardProps) => {
   const screenWidth = useGetScreenWidth();
   const isTablet = screenWidth < UIConstants.sizes.tabletWidth;
-  const ratingImageSrcString: string = `/src/assets/images/ratings/rating${restaurant.rating}.svg`;
+  const ratingsMap = new Map<number, string>();
+  ratingsMap.set(1, oneStarRating);
+  ratingsMap.set(2, twoStarRating);
+  ratingsMap.set(3, threeStarRating);
+  ratingsMap.set(4, fourStarRating);
+  ratingsMap.set(5, fiveStarRating);
 
   return (
-    <button
-      className={`${styles.restaurantCardContainer} ${
-        className ? styles[className] : ""
-      }`}
-    >
+    <button className={`${styles.restaurantCardContainer} ${className ?? ""}`}>
       <div className={styles.restaurantCardImageContainer}>
         <img src={restaurant.pictureUrl} alt={restaurant.name} />
       </div>
@@ -28,7 +35,7 @@ const RestaurantCard = ({ restaurant, className }: RestaurantCardProps) => {
           {!isTablet && (
             <div className={styles.restaurantRatingContainer}>
               <img
-                src={ratingImageSrcString}
+                src={ratingsMap.get(restaurant.rating)}
                 alt="rating"
                 className="rating-img"
               />
