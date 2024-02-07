7e4718e9f09432cd9c8d6fddc152d1ca2c932260
Oren Partovi <orenp@moveo.co.il>
2024-02-07 18:10:31 +0200

    remove of redundent prop restaurant card

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -11,7 +11,6 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 interface RestaurantCardProps {
   restaurant: Restaurant;
-  className?: string;
   cardWidth?: React.CSSProperties;
 }
 
