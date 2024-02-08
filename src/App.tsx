3475b5bec149df932c3a75b9a8285cb3d3780a5f
Oren Partovi <orenp@moveo.co.il>
2024-02-08 17:42:05 +0200

    App routes fixed

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         path: appRoutes.chefs,
       },
       {
-        path: "/restaurants/:id",
+        path: appRoutes.getRestaurantRoute(":id"),
         element: <RestaurantDisplayPage />,
       },
     ],
