a0b2de93ebd35a26a270ea3085acf09fca411f75
Oren Partovi <orenp@moveo.co.il>
2024-03-07 21:04:01 +0200

    go to all restaurants bug fix

--- src/components/GoToAllRestaurantsButton/GoToAllRestaurantsButton.tsx
+++ src/components/GoToAllRestaurantsButton/GoToAllRestaurantsButton.tsx
@@ -1,16 +1,16 @@
 import styles from "./GoToAllRestaurantsButton.module.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { appRoutes } from "../../shared/constants";
 import { Icons } from "../../assets/images";
 
 const GoToAllRestaurantsButton = () => {
   return (
-    <Link to={appRoutes.restaurants.base} className={styles.linkContainer}>
+    <NavLink to={appRoutes.restaurants.base} className={styles.linkContainer}>
       <button className={styles.gotoAllRestaurantsBtn} name="all-restaurant-goto-button">
         <p className={styles.gotoAllRestaurantsText}>All Restaurants</p>
         <img src={Icons.goToIcon} alt="go-to-icon" />
       </button>
-    </Link>
+    </NavLink>
   );
 };
 
