18691782fce67ad0cf6918348eab0b8ae021017e
Oren Partovi <orenp@moveo.co.il>
2024-03-07 20:04:47 +0200

    fixed infinite scroll

--- src/pages/RestaurantsPage/AllRestaurants/AllRestaurants.tsx
+++ src/pages/RestaurantsPage/AllRestaurants/AllRestaurants.tsx
@@ -2,24 +2,20 @@ import { FC } from "react";
 import { AppDispatch, RootState } from "../../../reduxToolkit/store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllRestaurants } from "../../../reduxToolkit/thunks/restaurantThunk";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "../../../components/RestaurantCard/RestaurantCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useOutletContext } from "react-router-dom";
 import { setAllRestaurants } from "../../../reduxToolkit/slices/restaurantSlice";
-import useIsMobile from "../../../hooks/useIsMobile";
-import useIsTablet from "../../../hooks/useIsTablet";
 import { Restaurant } from "../../../types/types";
 
 const AllRestaurants: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { allRestaurants, ratingFilter } = useSelector((state: RootState) => state.restaurant);
-  const containerRef = useRef<HTMLDivElement | null>(null);
   const [renderRestaurant, setRenderRestaurant] = useState<Restaurant[]>();
+  const [page, setPage] = useState(1);
 
-  const isTablet = useIsTablet();
-  const isMobile = useIsMobile();
-  const isMobileOrTablet = isMobile || isTablet;
+  const restaurantCardWidth = 335;
   const classN: string = useOutletContext();
 
   useEffect(() => {
@@ -30,21 +26,6 @@ const AllRestaurants: FC = () => {
     }
   }, [ratingFilter, allRestaurants]);
 
-  const [page, setPage] = useState(1);
-
-  const marginError = 10;
-  const handleScroll = () => {
-    if (containerRef.current) {
-      if (Math.abs(containerRef.current.scrollTop - (containerRef.current.scrollHeight - containerRef.current.clientHeight)) <= marginError) {
-        fetchMoreData();
-        containerRef.current.scrollTo({
-          top: containerRef.current.scrollTop + 150,
-          behavior: "smooth",
-        });
-      }
-    }
-  };
-
   useEffect(() => {
     dispatch(setAllRestaurants([]));
   }, []);
@@ -53,21 +34,13 @@ const AllRestaurants: FC = () => {
     dispatch(fetchAllRestaurants(page.toString()));
   }, [dispatch, page]);
 
-  const restaurantCardWidth = 335;
-
   const fetchMoreData = () => {
     setPage(page + 1);
   };
 
-  return !isMobileOrTablet ? (
-    <div className={classN} ref={containerRef} onScroll={handleScroll}>
-      {renderRestaurant?.map((restaurant) => (
-        <RestaurantCard restaurant={restaurant} key={restaurant._id} cardWidth={{ width: `${restaurantCardWidth}px` }} />
-      ))}
-    </div>
-  ) : (
-    <InfiniteScroll dataLength={renderRestaurant.length} next={fetchMoreData} hasMore={true} loader={""} className="">
-      <div style={{ display: "flex", flexDirection: "column", gap: "25px" }}>
+  return (
+    <InfiniteScroll dataLength={renderRestaurant ? renderRestaurant.length : 0} next={fetchMoreData} hasMore={true} loader={""}>
+      <div className={classN}>
         {renderRestaurant?.map((restaurant) => (
           <RestaurantCard restaurant={restaurant} key={restaurant._id} cardWidth={{ width: `${restaurantCardWidth}px` }} />
         ))}
--- src/pages/RestaurantsPage/NewRestaurants/NewRestaurants.tsx
+++ src/pages/RestaurantsPage/NewRestaurants/NewRestaurants.tsx
@@ -2,19 +2,19 @@ import { FC } from "react";
 import { AppDispatch, RootState } from "../../../reduxToolkit/store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNewRestaurants } from "../../../reduxToolkit/thunks/restaurantThunk";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "../../../components/RestaurantCard/RestaurantCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useOutletContext } from "react-router-dom";
 import { setNewRestaurants } from "../../../reduxToolkit/slices/restaurantSlice";
-import useIsMobile from "../../../hooks/useIsMobile";
-import useIsTablet from "../../../hooks/useIsTablet";
 import { Restaurant } from "../../../types/types";
 
 const NewRestaurants: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const restaurantCardWidth = 335;
+
   const { newRestaurants, ratingFilter } = useSelector((state: RootState) => state.restaurant);
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [page, setPage] = useState(1);
   const [renderRestaurant, setRenderRestaurant] = useState<Restaurant[]>();
 
   const classN: string = useOutletContext();
@@ -27,25 +27,6 @@ const NewRestaurants: FC = () => {
     }
   }, [ratingFilter, newRestaurants]);
 
-  const isTablet = useIsTablet();
-  const isMobile = useIsMobile();
-  const isMobileOrTablet = isMobile || isTablet;
-
-  const [page, setPage] = useState(1);
-
-  const marginError = 10;
-  const handleScroll = () => {
-    if (containerRef.current) {
-      if (Math.abs(containerRef.current.scrollTop - (containerRef.current.scrollHeight - containerRef.current.clientHeight)) <= marginError) {
-        fetchMoreData();
-        containerRef.current.scrollTo({
-          top: containerRef.current.scrollTop + 150,
-          behavior: "smooth",
-        });
-      }
-    }
-  };
-
   useEffect(() => {
     dispatch(setNewRestaurants([]));
   }, []);
@@ -54,21 +35,13 @@ const NewRestaurants: FC = () => {
     dispatch(fetchNewRestaurants(page.toString()));
   }, [dispatch, page]);
 
-  const restaurantCardWidth = 335;
-
   const fetchMoreData = () => {
     setPage(page + 1);
   };
 
-  return !isMobileOrTablet ? (
-    <div className={classN} ref={containerRef} onScroll={handleScroll}>
-      {renderRestaurant?.map((restaurant) => (
-        <RestaurantCard restaurant={restaurant} key={restaurant._id} cardWidth={{ width: `${restaurantCardWidth}px` }} />
-      ))}
-    </div>
-  ) : (
-    <InfiniteScroll dataLength={renderRestaurant.length} next={fetchMoreData} hasMore={true} loader={""} className="">
-      <div style={{ display: "flex", flexDirection: "column", gap: "25px" }}>
+  return (
+    <InfiniteScroll dataLength={renderRestaurant ? renderRestaurant.length : 0} next={fetchMoreData} hasMore={true} loader={""}>
+      <div className={classN}>
         {renderRestaurant?.map((restaurant) => (
           <RestaurantCard restaurant={restaurant} key={restaurant._id} cardWidth={{ width: `${restaurantCardWidth}px` }} />
         ))}
