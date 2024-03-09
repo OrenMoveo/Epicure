344ba960484c7c6476f1b5719c98b3fbd4326b3d
Oren Partovi <orenp@moveo.co.il>
2024-03-09 09:31:26 +0200

    chefs restaurant card link

--- src/components/ChefsRestaurantCard/ChefsRestaurantCard.tsx
+++ src/components/ChefsRestaurantCard/ChefsRestaurantCard.tsx
@@ -1,21 +1,26 @@
 import styles from "./ChefsRestaurantCard.module.scss";
 import { Restaurant } from "../../types/types";
+import { Link } from "react-router-dom";
+import { FC } from "react";
+import { appRoutes } from "../../shared/constants";
 interface ChefsRestaurantCardProps {
-  restaurant: Restaurant | undefined;
+  restaurant: Restaurant ;
 }
 
-const ChefsRestaurantCard = ({ restaurant }: ChefsRestaurantCardProps) => {
+const ChefsRestaurantCard :FC<ChefsRestaurantCardProps>= ({ restaurant }) => {
   return (
-    <div className={styles.cardContainer}>
+    <Link to={appRoutes.restaurants.getRestaurantRoute(restaurant._id)}>
+    <button className={styles.cardContainer}>
       <div className={styles.cardImageContainer}>
-        <img src={restaurant?.pictureUrl} alt={restaurant?.name} />
+        <img src={restaurant.pictureUrl} alt={restaurant.name} />
       </div>
       <div className={styles.restaurantContentLayout}>
         <div className={styles.restaurantContentContainer}>
-          <p className={styles.restaurantTitle}>{restaurant?.name}</p>
+          <p className={styles.restaurantTitle}>{restaurant.name}</p>
         </div>
       </div>
-    </div>
+    </button>
+    </Link>
   );
 };
 
